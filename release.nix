439afa9f1c0c1db46e5347406de382912330f506
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-09-12 12:28:06 +0200

    Restructure repository, add release.nix script

---
+++ release.nix
@@ -0,0 +1,30 @@
+let
+  pkgs = import <nixpkgs> {};
+
+in {
+  canary = { n, commit }: pkgs.stdenv.mkDerivation {
+    name = "avers";
+    src = ./.;
+
+    buildInputs = [
+      pkgs.nodejs-10_x
+      pkgs.jq
+    ];
+
+    buildPhase = ''
+      HOME=$PWD npm install
+      ./node_modules/.bin/tsc
+      cat package.json | jq 'del(.devDependencies) | .version = .version + "-alpha.${n}+${commit}"' > dist/package.json
+    '';
+
+    checkPhase = ''
+      ./node_modules/.bin/tslint --project .
+      ./node_modules/.bin/mocha dist
+    '';
+
+    installPhase = ''
+      mv dist $out
+      rm $out/*.test.*
+    '';
+  };
+}
