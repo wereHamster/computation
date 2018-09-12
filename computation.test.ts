85c1198b1b44f4f9b7d2f083e40859b59852da53
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-09-12 12:02:56 +0200

    Reformat computation.ts

--- computation.test.ts
+++ computation.test.ts
@@ -1,83 +1,91 @@
 import Computation from "./computation";
-import { assert }  from "chai";
-
+import { assert } from "chai";
 
 // Some predefined computations.
-var fortyTwo = Computation.pure(42)
-  , pending  = <any> Computation.pending
-  , failure  = <any> Computation.fail(new Error('fail'));
-
+var fortyTwo = Computation.pure(42),
+  pending = <any>Computation.pending,
+  failure = <any>Computation.fail(new Error("fail"));
 
 // Transformation functions.
 function multiplyByTwo(x) {
-    return x * 2;
+  return x * 2;
 }
 
 function multipleByTwoC(x) {
-    return Computation.pure(x * 2);
+  return Computation.pure(x * 2);
 }
 
-
-describe('Computation#get', function() {
-    it('should use the given computation function to compute the result', function() {
-        assert.equal(42, fortyTwo.get(undefined));
-    });
-    it('should use the fallback value if the computation is pending', function() {
-        assert.equal('fallback', pending.get('fallback'));
-    });
-    it('should use the fallback value if the computation throws an exception', function() {
-        assert.equal('fallback', failure.get('fallback'));
-    });
+describe("Computation#get", function() {
+  it("should use the given computation function to compute the result", function() {
+    assert.equal(42, fortyTwo.get(undefined));
+  });
+  it("should use the fallback value if the computation is pending", function() {
+    assert.equal("fallback", pending.get("fallback"));
+  });
+  it("should use the fallback value if the computation throws an exception", function() {
+    assert.equal("fallback", failure.get("fallback"));
+  });
 });
 
-describe('Computation#then', function() {
-    it('should use the resolve function to map the value', function() {
-        assert.equal(84, fortyTwo.then(multiplyByTwo).get(undefined));
-    });
-    it('should fail if the computation throws an exception', function() {
-        assert.equal(42, failure.then(undefined).get(42));
-    });
-    it('should should use the reject callback to transform exceptions', function() {
-        function handleError(e) { return 'not ' + e.message; }
-        assert.equal('not fail', failure.then(null, handleError).get(undefined));
-    });
+describe("Computation#then", function() {
+  it("should use the resolve function to map the value", function() {
+    assert.equal(84, fortyTwo.then(multiplyByTwo).get(undefined));
+  });
+  it("should fail if the computation throws an exception", function() {
+    assert.equal(42, failure.then(undefined).get(42));
+  });
+  it("should should use the reject callback to transform exceptions", function() {
+    function handleError(e) {
+      return "not " + e.message;
+    }
+    assert.equal("not fail", failure.then(null, handleError).get(undefined));
+  });
 });
 
-describe('Computation#fmap', function() {
-    it('should map the result to a new value', function() {
-        assert.equal(84, fortyTwo.fmap(multiplyByTwo).get(undefined));
-    });
-    it('should pass pending state through', function() {
-        assert.equal(42, pending.fmap(undefined).get(42));
-    });
+describe("Computation#fmap", function() {
+  it("should map the result to a new value", function() {
+    assert.equal(84, fortyTwo.fmap(multiplyByTwo).get(undefined));
+  });
+  it("should pass pending state through", function() {
+    assert.equal(42, pending.fmap(undefined).get(42));
+  });
 });
 
-describe('Computation#bind', function() {
-    it('should propagate pending state of the continuation', function() {
-        assert.equal('pending', fortyTwo.bind(pending).get('pending'));
-    });
-    it('should propagate error of the continuation', function() {
-        assert.equal('pending', fortyTwo.bind(failure).get('pending'));
-    });
-    it('should apply the function to the value', function() {
-        assert.equal(84, fortyTwo.bind(multipleByTwoC).get(undefined));
-    });
+describe("Computation#bind", function() {
+  it("should propagate pending state of the continuation", function() {
+    assert.equal("pending", fortyTwo.bind(pending).get("pending"));
+  });
+  it("should propagate error of the continuation", function() {
+    assert.equal("pending", fortyTwo.bind(failure).get("pending"));
+  });
+  it("should apply the function to the value", function() {
+    assert.equal(84, fortyTwo.bind(multipleByTwoC).get(undefined));
+  });
 });
 
-describe('Computation#liftA2', function() {
-    it('should pass input to the given function', function() {
-        assert.equal(84, Computation.liftA2(fortyTwo, fortyTwo, function(a, b) {
-            return a + b;
-        }).get(undefined));
-    });
-    it('should propagate pending state', function() {
-        assert.equal('pending', Computation.liftA2(pending, fortyTwo, function(a, b) {
-            return '' + a + b;
-        }).get('pending'));
-    });
-    it('should propagate errors', function() {
-        assert.equal('pending', Computation.liftA2(failure, fortyTwo, function(a, b) {
-            return '' + a + b;
-        }).get('pending'));
-    });
+describe("Computation#liftA2", function() {
+  it("should pass input to the given function", function() {
+    assert.equal(
+      84,
+      Computation.liftA2(fortyTwo, fortyTwo, function(a, b) {
+        return a + b;
+      }).get(undefined)
+    );
+  });
+  it("should propagate pending state", function() {
+    assert.equal(
+      "pending",
+      Computation.liftA2(pending, fortyTwo, function(a, b) {
+        return "" + a + b;
+      }).get("pending")
+    );
+  });
+  it("should propagate errors", function() {
+    assert.equal(
+      "pending",
+      Computation.liftA2(failure, fortyTwo, function(a, b) {
+        return "" + a + b;
+      }).get("pending")
+    );
+  });
 });
--- computation.ts
+++ computation.ts
@@ -3,99 +3,109 @@
 // fetched from a server).
 
 export default class Computation<T> {
+  // Special value which can be used to denote that the computation is
+  // pending and the result may become available at a later time.
+  //
+  // We use a runtime-wide Symbol. The 'Computation' library may be loaded
+  // by multiple node modules independently, but we need to ensure that the
+  // 'Pending' value can be compared for equality.
 
-    // Special value which can be used to denote that the computation is
-    // pending and the result may become available at a later time.
-    //
-    // We use a runtime-wide Symbol. The 'Computation' library may be loaded
-    // by multiple node modules independently, but we need to ensure that the
-    // 'Pending' value can be compared for equality.
+  static Pending: any = Symbol.for("Computation.Pending");
 
-    static Pending : any = Symbol.for('Computation.Pending');
+  // The function which when called will produce the computation result.
+  private fn: () => T;
 
+  constructor(fn: () => T) {
+    this.fn = fn;
+  }
 
-    // The function which when called will produce the computation result.
-    private fn : () => T;
+  // Convenience functions to create pure and failing computations.
+  static pure<V>(value: V) {
+    return new Computation(() => value);
+  }
 
+  static fail<V>(e: Error) {
+    return new Computation(
+      (): V => {
+        throw e;
+      }
+    );
+  }
 
-    constructor(fn: () => T) {
-        this.fn = fn;
-    }
+  // A predefined computation which is always pending. It is a property
+  // rather than a function because it doesn't have to be parametrized.
+  static pending = new Computation(() => <any>Computation.Pending);
 
-
-
-    // Convenience functions to create pure and failing computations.
-    static pure<V>(value: V) { return new Computation(() => value); }
-    static fail<V>(e: Error) { return new Computation((): V => { throw e; }); }
-
-    // A predefined computation which is always pending. It is a property
-    // rather than a function because it doesn't have to be parametrized.
-    static pending = new Computation(() => <any> Computation.Pending);
-
-
-    // Like the ES6 Promise#then function.
-    then<V>(resolve: (value: T) => V, reject?: (err: Error) => V): Computation<V> {
-        if (reject === undefined) {
-            return new Computation(() => resolve(this.fn()));
-        } else {
-            return new Computation(() => {
-                try {
-                    return resolve(this.fn());
-                } catch (e) {
-                    return reject(e);
-                }
-            });
-        }
-    }
-
-    // Map over the result. Pending state and errors are passsed onto the next
-    // computation untounched.
-    fmap<V>(f: (value: T) => V): Computation<V> {
-        return this.then(v => {
-            if (v === Computation.Pending) {
-                return <V> Computation.Pending;
-            } else {
-                return f(v);
-            }
-        });
-    }
-
-    // Like fmap, but the function can return a computation which is then
-    // automatically executed.
-    bind<V>(f: (value: T) => Computation<V>): Computation<V> {
-        return this.fmap(v => f(v).fn());
-    }
-
-
-    // Pending computations and errors are passed through.
-    static liftA2<A,B,C>(a: Computation<A>, b: Computation<B>, f: (a: A, b: B) => C): Computation<C> {
-        return new Computation(() => {
-            let av = a.fn(), bv = b.fn();
-            if (av !== Computation.Pending && bv !== Computation.Pending) {
-                return f(av, bv);
-            } else {
-                return Computation.Pending;
-            }
-        });
-    }
-
-    // Get the result of this computation. If the result is not available yet,
-    // return the fallback value.
-    get(fallback: T): T {
-        return this.getf(() => fallback);
-    }
-
-    // Like 'get' but the fallback value is created lazily.
-    getf(fallback: () => T): T {
+  // Like the ES6 Promise#then function.
+  then<V>(
+    resolve: (value: T) => V,
+    reject?: (err: Error) => V
+  ): Computation<V> {
+    if (reject === undefined) {
+      return new Computation(() => resolve(this.fn()));
+    } else {
+      return new Computation(() => {
         try {
-            let result = this.fn();
-            if (result === Computation.Pending) {
-                return fallback();
-            } else {
-                return result;
-            }
+          return resolve(this.fn());
         } catch (e) {
-            return fallback();
+          return reject(e);
         }
+      });
     }
+  }
+
+  // Map over the result. Pending state and errors are passsed onto the next
+  // computation untounched.
+  fmap<V>(f: (value: T) => V): Computation<V> {
+    return this.then(v => {
+      if (v === Computation.Pending) {
+        return <V>Computation.Pending;
+      } else {
+        return f(v);
+      }
+    });
+  }
+
+  // Like fmap, but the function can return a computation which is then
+  // automatically executed.
+  bind<V>(f: (value: T) => Computation<V>): Computation<V> {
+    return this.fmap(v => f(v).fn());
+  }
+
+  // Pending computations and errors are passed through.
+  static liftA2<A, B, C>(
+    a: Computation<A>,
+    b: Computation<B>,
+    f: (a: A, b: B) => C
+  ): Computation<C> {
+    return new Computation(() => {
+      let av = a.fn(),
+        bv = b.fn();
+      if (av !== Computation.Pending && bv !== Computation.Pending) {
+        return f(av, bv);
+      } else {
+        return Computation.Pending;
+      }
+    });
+  }
+
+  // Get the result of this computation. If the result is not available yet,
+  // return the fallback value.
+  get(fallback: T): T {
+    return this.getf(() => fallback);
+  }
+
+  // Like 'get' but the fallback value is created lazily.
+  getf(fallback: () => T): T {
+    try {
+      let result = this.fn();
+      if (result === Computation.Pending) {
+        return fallback();
+      } else {
+        return result;
+      }
+    } catch (e) {
+      return fallback();
+    }
+  }
 }
